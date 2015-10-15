5568bba028321b190b215b7fc4eeefb631819156
a3kuni <houdig@gmail.com>
2015-10-15 13:50:45 +0000

    Change migration files written by PC

--- db/migrate/20151011052420_change_textbook_to_studysession.rb
+++ db/migrate/20151011052420_change_textbook_to_studysession.rb
@@ -1,11 +1,11 @@
 class ChangeTextbookToStudysession < ActiveRecord::Migration
     #変更後の型
   def up
-    change_column :Studysessions, :textbook, :string
+    change_column :studysessions, :textbook, :string
   end
 
   #変更前の型
   def down
-    change_column :Studysessions, :textbook, :intger
+    change_column :studysessions, :textbook, :intger
   end
 end
