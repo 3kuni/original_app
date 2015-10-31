9b1c886bd4ca5cc84f701967ce61c78a15a9dc5e
a3kuni <houdig@gmail.com>
2015-10-31 01:33:35 +0000

    Use partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    if request.url == "http://stardy-3kuni.sqale.jp/"
-      redirect_to "http://www.stardy.co/"
-    end
     if signed_in?
       @active_now=Studysession.find_by(user:current_user.id,active:true)
       @user=User.find(current_user.id)
-      @feed_items=current_user.feed
+      @feeditems=current_user.feed
       @activities = PublicActivity::Activity.order(created_at: :desc).all
     end
   end
