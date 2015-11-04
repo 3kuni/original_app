7ddb8ac852a2fc0d7dceedb9ff43c69c42f70989
三國清志 <a3kuni@mikunikiyoshi-no-MacBook-Pro.local>
2015-11-04 11:13:31 +0900

    Add migration for skype

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103021848) do
+ActiveRecord::Schema.define(version: 20151104021259) do
 
   create_table "Studysessions", force: :cascade do |t|
     t.integer  "user"
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(version: 20151103021848) do
     t.string   "word"
     t.string   "grade"
     t.string   "area"
+    t.string   "skype"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
