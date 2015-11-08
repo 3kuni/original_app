e087634588fb80e73f31fa4d7bdbbc262f78d57c
三國清志 <a3kuni@mikunikiyoshi-no-MacBook-Pro.local>
2015-11-08 13:10:25 +0900

    Fix over 3hour session using with cron

--- lib/tasks/timeover.rb
+++ lib/tasks/timeover.rb
@@ -2,7 +2,7 @@ class Timeover
   def self.kill
     @current_active=Studysession.where(active:true)  
     @current_active.each do |i|
-      if (Time.now-i.created_at) > 1
+      if (Time.now-i.created_at) > 10800
         i.update_attributes(active:false)
         current_students=Room.find(i.room).current_students
         Room.find(i.room).update_attributes(current_students:current_students-1)
