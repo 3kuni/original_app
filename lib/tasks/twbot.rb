909d6a10a37d97a93718e8648576af0fa93c20e0
三國清志 <a3kuni@mikunikiyoshi-no-MacBook-Pro.local>
2016-01-06 10:48:16 +0900

    Modify secret_key_base:

--- lib/tasks/twbot.rb
+++ lib/tasks/twbot.rb
@@ -14,7 +14,7 @@ class Twbot
     result_tweets = client.search(query, count: 10, result_type: "recent",  exclude: "retweets", since_id: since_id, to: "stardyjp")
     result_tweets.take(10).each_with_index do |tw, i| 
       puts "#{i}: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}" 
-      #client.update("@#{tw.user.screen_name} ふぁいてぃん！", in_reply_to_status_id: tw.id) 
+      #client.update("@#{tw.user.screen_name} ふぁいてぃん！", in_reply_to_status_id: tw.id)
     end
     #puts result_tweets
     #client.update("@_3kuni ファイト～", in_reply_to_status_id: 684367796742955008)
