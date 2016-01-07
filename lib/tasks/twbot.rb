ad0143eddd34ba2597594131d99acfc837d22004
三國清志 <a3kuni@mikunikiyoshi-no-MacBook-Pro.local>
2016-01-07 12:12:29 +0900

    Fix double reply

--- lib/tasks/twbot.rb
+++ lib/tasks/twbot.rb
@@ -8,16 +8,29 @@ class Twbot
       access_token:        ENV['tw_access_token'],
       access_token_secret: ENV['tw_access_token_secret'],
     )
-  
+
+    last_update=nil
+    
+    f = File.open('./config/twbot_settings.yml', 'r')
+    since_id = f.to_a[0].to_i
+    f.close
     query = "勉強しよ "
-    since_id = nil
-    result_tweets = client.search(query, count: 10, result_type: "recent",  exclude: "retweets", since_id: since_id, to: "stardyjp")
+    result_tweets = client.search(query, count: 10, result_type: "recent",  exclude: "retweets", since_id: since_id, to: "benkyo_stardy")
     result_tweets.take(10).each_with_index do |tw, i| 
       puts "#{i}: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}" 
       client.update("@#{tw.user.screen_name} ふぁいてぃん！", in_reply_to_status_id: tw.id)
+      if i==0 
+        last_update = tw.id
+      end
     end
     #puts result_tweets
     #client.update("@_3kuni ファイト～", in_reply_to_status_id: 684367796742955008)
+    unless last_update.nil?
+      f = File.open('./config/twbot_settings.yml', 'w') # wは書き込み権限
+      f.puts last_update
+      f.close
+    end
+    
   end
 
   def self.kill
