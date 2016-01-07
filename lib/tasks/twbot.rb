68f0fdd286e8b11d1b4d6162d19896e6b4c9518e
三國清志 <a3kuni@mikunikiyoshi-no-MacBook-Pro.local>
2016-01-07 13:51:25 +0900

    Modify #{ENV}

--- lib/tasks/twbot.rb
+++ lib/tasks/twbot.rb
@@ -5,10 +5,11 @@ class Twbot
     client = Twitter::REST::Client.new(
       consumer_key:        ENV['tw_consumer_key'] ,
       consumer_secret:     ENV['tw_consumer_secret'] ,
-      access_token:        ENV['tw_access_token'],
+      access_token:        "#{ENV['tw_access_token']}",
       access_token_secret: ENV['tw_access_token_secret'],
     )
-
+    puts "#{ENV['tw_access_token']}"
+    puts ENV['tw_access_token']
     last_update=nil
     
     f = File.open('./config/twbot_settings.yml', 'r')
@@ -18,13 +19,11 @@ class Twbot
     result_tweets = client.search(query, count: 10, result_type: "recent",  exclude: "retweets", since_id: since_id, to: "benkyo_stardy")
     result_tweets.take(10).each_with_index do |tw, i| 
       puts "#{i}: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}" 
-      client.update("@#{tw.user.screen_name} ふぁいてぃん！", in_reply_to_status_id: tw.id)
+      #client.update("@#{tw.user.screen_name} ふぁいてぃん！", in_reply_to_status_id: tw.id)
       if i==0 
         last_update = tw.id
       end
     end
-    #puts result_tweets
-    #client.update("@_3kuni ファイト～", in_reply_to_status_id: 684367796742955008)
     unless last_update.nil?
       f = File.open('./config/twbot_settings.yml', 'w') # wは書き込み権限
       f.puts last_update
