1bfffce53f5d1d5b1f5f12b2e0082d04d2e94e6d
三國清志 <a3kuni@mikunikiyoshi-no-MacBook-Pro.local>
2016-01-08 22:57:44 +0900

    Add stop from twitter

--- lib/tasks/twbot.rb
+++ lib/tasks/twbot.rb
@@ -9,33 +9,50 @@ class Twbot
       access_token:        "#{ENV['tw_access_token']}",
       access_token_secret: ENV['tw_access_token_secret'],
     )
-    puts "#{ENV['tw_access_token']}"
-    puts ENV['tw_access_token']
-    last_update=nil
+
     
     f = File.open('./config/twbot_settings.yml', 'r')
-    since_id = f.to_a[0].to_i
+    since =f.readlines
+    since_start_id = since[0]
+    since_stop_id = since[1]
     f.close
-    query = "勉強しよ "
-    result_tweets = client.search(query, count: 10, result_type: "recent",  exclude: "retweets", since_id: since_id, to: "benkyo_stardy")
+    last_update_start=since_start_id
+    last_update_stop=since_stop_id
+    puts "since_start_id #{since_start_id}"
+    puts "since_stop_id #{since_stop_id}"
+    query_start = "勉強しよ "
+    result_tweets = client.search(query_start, count: 10, result_type: "recent",  exclude: "retweets", since_id: since_start_id, to: "benkyo_stardy")
     result_tweets.take(10).each_with_index do |tw, i| 
-      puts "#{i}: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}" 
+      puts "START: #{i}: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}" 
       stardy_user = User.find_by(provider:"twitter",name:tw.user.screen_name)
       if stardy_user.present?
         #puts "stardy_nickname: #{stardy_user.nickname},#{stardy_user.area},#{stardy_user.grade},#{stardy_user.word}"
         @studysession=Studysession.new(user: stardy_user.id, room: "1", textbook: "勉強")
         @studysession.save
       end
-      client.update("@#{tw.user.screen_name} ふぁいてぃん！", in_reply_to_status_id: tw.id)
+      #client.update("@#{tw.user.screen_name} ふぁいてぃん！", in_reply_to_status_id: tw.id)
       if i==0 
-        last_update = tw.id
+        last_update_start = tw.id
       end
     end
-    unless last_update.nil?
-      f = File.open('./config/twbot_settings.yml', 'w') # wは書き込み権限
-      f.puts last_update
-      f.close
+    query_stop = "勉強おわ "
+    result_tweets = client.search(query_stop, count: 10, result_type: "recent",  exclude: "retweets", since_id: since_stop_id, to: "benkyo_stardy")
+    result_tweets.take(10).each_with_index do |tw, i| 
+      puts "STOP: #{i}: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}" 
+      stardy_user = User.find_by(provider:"twitter",name:tw.user.screen_name)
+      if stardy_user.present?
+
+      end
+      client.update("@#{tw.user.screen_name} おつかれ〜", in_reply_to_status_id: tw.id)
+      if i==0 
+        last_update_stop = tw.id
+      end
     end
+
+    f = File.open('./config/twbot_settings.yml', 'w') # wは書き込み権限
+    f.puts last_update_start
+    f.puts last_update_stop
+    f.close
     
   end
 
