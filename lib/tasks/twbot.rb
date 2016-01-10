cb69849f6dba9d654c06d3cf259f9f1b412173e9
三國清志 <a3kuni@mikunikiyoshi-no-MacBook-Pro.local>
2016-01-10 20:39:11 +0900

    Add register textbook

--- lib/tasks/twbot.rb
+++ lib/tasks/twbot.rb
@@ -10,6 +10,8 @@ class Twbot
       access_token_secret: ENV['tw_access_token_secret'],
     )
 
+
+
     f = File.open('./config/twbot_settings.yml', 'r')
     since =f.readlines
     since_start_id = since[0]
@@ -25,13 +27,19 @@ class Twbot
       puts "START: #{i}: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}" 
       stardy_user = User.find_by(provider:"twitter",name:tw.user.screen_name)
       if stardy_user.present?
-        @studysession = Studysession.new(user: stardy_user.id, room: "1", textbook: "勉強",active:true)
+        textbook = tw.full_text.match(/@benkyo_stardy\s勉強しよ[[:blank:]]+(?<text>.+)/)
+        if textbook.present?
+          textbook = textbook[:text]
+        else
+          textbook = "勉強"
+        end
+        @studysession = Studysession.new(user: stardy_user.id, room: "1", textbook: textbook,active:true)
         @studysession.save
         @room = Room.find(1)
         @room.update_attributes(current_students:@room.current_students.to_i+1)
       end
-      client.update("@#{tw.user.screen_name} ふぁいてぃん！(*•̀ᴗ•́*)و ̑̑", in_reply_to_status_id: tw.id)
-      client.favorite(tw.id)
+      client.update("@#{tw.user.screen_name} ふぁいてぃん！(*•̀ᴗ•́*)و ̑̑", in_reply_to_status_id: tw.id) if Rails.env == 'production'
+      client.favorite(tw.id) if Rails.env == 'production'
       if i==0 
         last_update_start = tw.id
       end
@@ -41,21 +49,23 @@ class Twbot
     result_tweets.take(10).each_with_index do |tw, i| 
       puts "STOP: #{i}: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}" 
       stardy_user = User.find_by(provider:"twitter",name:tw.user.screen_name)
-      stardy_active_session = Studysession.find_by(user: stardy_user.id,active: true)
-      if stardy_user.present? && stardy_active_session.present?
-        puts "Now active!"
-        time_minutes=(Time.now.to_i-stardy_active_session.created_at.to_i)/60
-        t_user=stardy_user.total_time.to_i + time_minutes
-        stardy_user.update_attributes(total_time:t_user)
-        stardy_active_session.update_attributes(active:false,time:time_minutes)
-        @room=Room.find(stardy_active_session.room)
-        t_room=@room.minutes_total.to_i + time_minutes
-        @room.update_attributes(minutes_total:t_room)
-        unless @room.current_students==0 
-          @room.update_attributes(current_students:@room.current_students-1)
+      if stardy_user.present?
+        stardy_active_session = Studysession.find_by(user: stardy_user.id,active: true)
+        if stardy_active_session.present?
+          puts "Now active!"
+          time_minutes=(Time.now.to_i-stardy_active_session.created_at.to_i)/60
+          t_user=stardy_user.total_time.to_i + time_minutes
+          stardy_user.update_attributes(total_time:t_user)
+          stardy_active_session.update_attributes(active:false,time:time_minutes)
+          @room=Room.find(stardy_active_session.room)
+          t_room=@room.minutes_total.to_i + time_minutes
+          @room.update_attributes(minutes_total:t_room)
+          unless @room.current_students==0 
+            @room.update_attributes(current_students:@room.current_students-1)
+          end
         end
       end
-      client.update("@#{tw.user.screen_name} おつかれ〜(๑´ω`ﾉﾉﾞ✧", in_reply_to_status_id: tw.id)
+      client.update("@#{tw.user.screen_name} おつかれ〜(๑´ω`ﾉﾉﾞ✧", in_reply_to_status_id: tw.id) if Rails.env == 'production'
       if i==0 
         last_update_stop = tw.id
       end
@@ -63,7 +73,10 @@ class Twbot
 
     f = File.open('./config/twbot_settings.yml', 'w') # wは書き込み権限
     f.puts last_update_start
+    #f.puts 686143010174713856
     f.puts last_update_stop
+    #f.puts 686142291103203329
+    
     f.close
     
   end
