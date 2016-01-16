34fd72827e71e9d3818c5f257607174d32c35400
三國清志 <a3kuni@mikunikiyoshi-no-MacBook-Pro.local>
2016-01-16 15:12:40 +0900

    Fix @feeditems in staticpagescontroller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    @feeditems = Studysession.all.limit(30)
     if user_signed_in?
       @active_now=Studysession.find_by(user:current_user.id,active:true)
       @user=User.find(current_user.id)
-      @feeditems=current_user.feed
+      #@feeditems=current_user.feed
       @activities = PublicActivity::Activity.order(created_at: :desc).all
     end
   end
