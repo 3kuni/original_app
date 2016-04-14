a694f1b9e61c4416a0049595878f7b7aa90c1f12
三國清志 <a3kuni@mikunikiyoshi-no-MacBook-Pro.local>
2016-04-14 23:23:19 +0900

    Modify FF outer

--- lib/tasks/twbot.rb
+++ lib/tasks/twbot.rb
@@ -15,14 +15,153 @@ class Twbot
     f = File.open('./config/twbot_settings.yml', 'r')
     since =f.readlines
     since_start_id = since[0]
-    since_stop_id = since[1]
     f.close
     last_update_start=since_start_id
     puts "since_start_id #{since_start_id}"
 
-    # 勉強開始
-    query_start = "勉強しよ "
-    #result_tweets = client.search(query_start, count: 10, result_type: "recent",  exclude: "retweets", since_id: since_start_id, to: "benkyo_stardy")
+    # 検索でFF外から
+    query_start = "勉強しよ OR 勉強おわ"
+    result_tweets_non_follower = client.search(query_start, count: 200, result_type: "recent",  exclude: "retweets", since_id: since_start_id, to: "benkyo_stardy")
+    first_search = true
+    result_tweets_non_follower.take(200).each do |tw|
+      # 検索する最新のidを記録
+      if first_search
+        last_update_start = tw.id
+        first_search = nil
+      end
+
+      # 「勉強しよ」の処理
+      if tw.full_text.match(/@benkyo_stardy(\n+|[[:blank:]]+)勉強しよ.*/).present?
+        puts "FF外から: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}"
+        # 未登録のユーザか判定
+        unless User.find_by(name:tw.user.screen_name)
+          # 未登録ならゲストユーザとして登録
+          User.create!(name: tw.user.screen_name,provider: "guest",uid: User.create_unique_string,
+                       email: User.create_unique_guest_email,password: User.create_unique_guest_password)
+          
+          # ツイートからテキスト・つぶやきを取得
+          option1 = tw.text.match(/@benkyo_stardy(\n+|[[:blank:]]+)勉強しよ(\n|[[:blank:]]+)(?<text>\S+)(\n|[[:blank:]]*)/)
+          option2 = tw.text.match(/@benkyo_stardy(\n+|[[:blank:]]+)勉強しよ(\n|[[:blank:]]+)\S+(\n|[[:blank:]]+)(?<tweet>\S+)/)
+          textbook = nil
+          tweet = nil
+          if option1.present?
+            textbook = option1[:text]
+            tweet = option2[:tweet] if option2.present?
+          else
+            textbook = "勉強"
+          end
+
+          # ユーザインスタンスを作成、セッションを開始
+          guest_user = User.find_by(name: tw.user.screen_name)
+          @studysession = Studysession.new(user: guest_user.id, room: "1", textbook: textbook, tweet: tweet,active: true)
+          @studysession.save
+          @studysession.create_activity :create, owner: guest_user
+          @room = Room.find(1)
+          @room.update_attributes(current_students:@room.current_students.to_i+1)
+
+          # はじめてのメッセージ
+          puts "登録して勉強開始しました！"
+          client.update("@#{tw.user.screen_name} はじめての勉強記録です！(*•̀ᴗ•́*)و ̑̑", in_reply_to_status_id: tw.id) if Rails.env == 'production'
+          client.favorite(tw.id) if Rails.env == 'production'
+          client.retweet(tw.id) if Rails.env == 'production'
+        else
+          if client.friendship?(client.user(), tw.user)
+            puts "exists"
+          else
+            
+            # ツイートからテキスト・つぶやきを取得
+            option1 = tw.text.match(/@benkyo_stardy(\n+|[[:blank:]]+)勉強しよ(\n|[[:blank:]]+)(?<text>\S+)(\n|[[:blank:]]*)/)
+            option2 = tw.text.match(/@benkyo_stardy(\n+|[[:blank:]]+)勉強しよ(\n|[[:blank:]]+)\S+(\n|[[:blank:]]+)(?<tweet>\S+)/)
+            textbook = nil
+            tweet = nil
+            if option1.present?
+              textbook = option1[:text]
+              tweet = option2[:tweet] if option2.present?
+            else
+              textbook = "勉強"
+            end
+
+            # ユーザインスタンスを作成、セッションを開始
+            stardy_user = User.find_by(name: tw.user.screen_name)
+            already_exist = Studysession.find_by(user: stardy_user.id,active: true)
+            # セッション中でないことを確認して.save
+            unless already_exist.present?
+              puts "勉強を開始しました"
+              @studysession = Studysession.new(user: stardy_user.id, room: "1", textbook: textbook, tweet: tweet,active: true)
+              @studysession.save
+              @studysession.create_activity :create, owner: stardy_user
+              @room = Room.find(1)
+              @room.update_attributes(current_students:@room.current_students.to_i+1)
+            end
+          end
+        end
+      end
+      # 「勉強おわ」の処理
+      if tw.full_text.match(/@benkyo_stardy(\n+|[[:blank:]]+)勉強おわ.*/).present?
+        puts "FF外から「おわ」: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}"
+        # 登録済みユーザか判定
+        if User.find_by(name:tw.user.screen_name)
+          # 登録済みの時
+          unless client.friendship?(client.user(), tw.user)
+            stardy_user = User.find_by(name:tw.user.screen_name)
+            time_minutes = nil
+            praise_word = nil
+            if stardy_user.present?
+              stardy_active_session = Studysession.find_by(user: stardy_user.id,active: true)
+              if stardy_active_session.present?
+                puts "Now active!"
+                # 時間、回数のカウント
+                time_minutes=(Time.now.to_i-stardy_active_session.created_at.to_i)/60
+                t_user=stardy_user.total_time.to_i + time_minutes
+                times = stardy_user.times.to_i + 1
+                # User,Studysessionの更新
+                stardy_user.update_attributes(total_time:t_user,times: times)
+                stardy_active_session.update_attributes(active:false,time:time_minutes)
+                # ほめリプ
+                if  times==1
+                  praise_word = "おめでとうございます！初めて記録しました！"
+                elsif stardy_user.total_time.to_i < (60*3) && t_user >= (60*3)
+                  praise_word =  "3時間突破！"
+                elsif times == 3
+                  praise_word =  "3回目の勉強記録です！この調子で継続できたらいいですね！！"
+                elsif stardy_user.total_time.to_i < (60*6) && t_user >= (60*6)
+                  praise_word =  "勉強記録が6時間を超えました！"
+                elsif times == 6
+                  praise_word =  "勉強を記録したのは6回目です！もう慣れてきましたか？？"
+                elsif stardy_user.total_time.to_i < (60*10) && t_user >= (60*10)
+                  praise_word =  "記録が10時間を超えました！すごい！"
+                elsif times == 10
+                  praise_word =  "10回目の記録です！習慣になってきましたね！！"
+                elsif stardy_user.total_time.to_i < (60*30) && t_user >= (60*30)
+                  praise_word =  "祝！30時間以上勉強しました！"
+                elsif (times/5).to_f.integer?
+                  praise_word =  "#{times}回記録しましたー！"
+                elsif (stardy_user.total_time.to_i/100).floor < (t_user/100).floor
+                  praise_word =  "#{((new_total_time.to_i/100)*100)/60}時間を超えました！！"     
+                end
+
+                # Roomの更新
+                @room=Room.find(stardy_active_session.room)
+                t_room=@room.minutes_total.to_i + time_minutes
+                @room.update_attributes(minutes_total:t_room)
+                unless @room.current_students==0 
+                  @room.update_attributes(current_students:@room.current_students-1)
+                end
+              end
+            end
+            puts "おわりましたー"
+            time_minutes = "勉強時間は#{time_minutes}分です！！" if time_minutes.present?
+            client.update("@#{tw.user.screen_name} #{praise_word}おつかれ〜(๑´ω`ﾉﾉﾞ✧ #{time_minutes}", in_reply_to_status_id: tw.id) if Rails.env == 'production'
+          end
+        else
+          client.update("@#{tw.user.screen_name} 「勉強しよ」とリプを送ってみてください！", in_reply_to_status_id: tw.id) if Rails.env == 'production'
+          puts "勉強しよでスタートです"
+        end
+      end
+    end
+    sleep(5)
+
+
     # STARDYのタイムラインを取得
     result_tweets = client.home_timeline(since_id: since_start_id,count: 200)
     first = true
@@ -61,7 +200,7 @@ class Twbot
               praise_word =  "10回目の記録です！習慣になってきましたね！！"
             elsif stardy_user.total_time.to_i < (60*30) && t_user >= (60*30)
               praise_word =  "祝！30時間以上勉強しました！"
-            elsif (times/5).to_f.integer?
+            elsif times % 5 == 0
               praise_word =  "#{times}回記録しましたー！"
             elsif (stardy_user.total_time.to_i/100).floor < (t_user/100).floor
               praise_word =  "#{((new_total_time.to_i/100)*100)/60}時間を超えました！！"     
@@ -105,6 +244,7 @@ class Twbot
           # 登録済みユーザ
           @studysession = Studysession.new(user: stardy_user.id, room: "1", textbook: textbook, tweet: tweet,active: true)
           already_exist = Studysession.find_by(user: stardy_user.id,active: true)
+          # セッション中でないことを確認して.save
           unless already_exist.present?
             @studysession.save
             @studysession.create_activity :create, owner: stardy_user
@@ -114,16 +254,11 @@ class Twbot
           client.update("@#{tw.user.screen_name} ふぁいてぃん！(*•̀ᴗ•́*)و ̑̑", in_reply_to_status_id: tw.id) if Rails.env == 'production'
           client.favorite(tw.id) if Rails.env == 'production'
           client.retweet(tw.id) if Rails.env == 'production'
-        else
-          # 未登録ならゲストユーザとして登録
-          User.create!(name: tw.user.screen_name,provider: "guest",uid:User.create_unique_string,email: User.create_unique_guest_email,password: User.create_unique_guest_password)
-          client.update("@#{tw.user.screen_name} ふぁい！(*•̀ᴗ•́*)و ̑̑", in_reply_to_status_id: tw.id) if Rails.env == 'production'
-          
         end
       end
 
       if first 
-        last_update_start = tw.id
+        last_update_start = tw.id if tw.id > last_update_start
         first=nil
         # 設定ファイルの更新
         f = File.open('./config/twbot_settings.yml', 'w') # wは書き込み権限
@@ -191,20 +326,23 @@ class Twbot
     f = File.open('./config/twbot_settings.yml', 'r')
     since =f.readlines
     since_start_id = since[0]
-    since_stop_id = since[1]
     f.close
-
-    # 自分のタイムラインを取得
-    client.home_timeline(since_id: since_start_id,count: 200).each do |tw|
-      puts "#{tw.user.screen_name}:#{tw.text}:#{tw.id}"  if tw.text.match(/@benkyo_stardy(\n+|[[:blank:]]+)勉強しよ.*/).present?
-      #puts tw.text
+    # 手動で設定
+    since_start_id = 720591998600609792
+    # FF外からのツイートの処理
+    query_start = "勉強しよ OR 勉強おわ"
+    result_tweets_non_follower = client.search(query_start, count: 200, result_type: "recent",  exclude: "retweets", since_id: since_start_id, to: "benkyo_stardy")
+    result_tweets_non_follower.take(200).each do |tw|
+      puts "FF外から: @#{tw.user.screen_name}: #{tw.full_text}: id[#{tw.id}]: #{tw.created_at}"
+      # 未登録のユーザか判定
+      unless User.find_by(name:tw.user.screen_name)
+        if client.friendship?(client.user(), tw.user)
+          puts "following"
+        else
+          puts "not following"
+        end
+      end
     end
-    (1..60).each do |i|
-      printf("%2s",60-i)
-      sleep(1)
-      printf "\e[1D\e[1D"
-    end
-
   end
 
 end
