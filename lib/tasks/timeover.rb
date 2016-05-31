44274c2a3508ae83eeed8508c1e4152ba3b47e95
三國清志 <houdig@gmail.com>
2016-06-01 00:12:17 +0900

    Add 170minutes alert

--- lib/tasks/timeover.rb
+++ lib/tasks/timeover.rb
@@ -22,6 +22,13 @@ class Timeover
           #client.update("@#{stardy_user.name} 3時間超えたので自動終了しました(・ω・)編集はここからwww.stardy.co/studysessions/edit/#{i.id}")
           #puts "KILLED @#{stardy_user.name} 3時間超えたので自動終了しました〜"
         end
+      elsif (Time.now-i.created_at) > 10200
+        stardy_user = User.find_by(id:i.user)
+        if stardy_user.present? && (stardy_user.provider == "twitter" || stardy_user.provider == "guest")
+          stardy_active_session = i
+          client.update("@#{stardy_user.name} あと10分で自動終了します(๑•́‧̫•̀๑) ")
+          #puts "KILLED @#{stardy_user.name} 3時間超えたので自動終了しました〜"
+        end
       elsif (Time.now-i.created_at) > 7200
         stardy_user = User.find_by(id:i.user)
         if stardy_user.present? && (stardy_user.provider == "twitter" || stardy_user.provider == "guest")
