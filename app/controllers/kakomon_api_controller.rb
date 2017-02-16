03200bb0d56b3f2c80cee6dde3654cc49fcce351
3kuni <houdig@gmail.com>
2017-02-16 13:48:39 +0900

    🛀Modify

--- app/controllers/kakomon_api_controller.rb
+++ app/controllers/kakomon_api_controller.rb
@@ -1,7 +1,9 @@
 require 'json'
 class KakomonApiController < ApplicationController
   def showYear
-    json = {"status" => "success", "message" => "success create new wcuser"}
+    # 表示する年度を返す。
+    json = {"status" => "success", "message" => "success get year",
+            "data" => [{"year"=>"2017","visible" => false},{"year"=>"2016","visible" => true}]}
     render :json =>  json
   end
 
