0876bc14a8dded326e3151c388186b9fc6a2198b
3kuni <houdig@gmail.com>
2017-02-21 16:58:35 +0900

    🛀Modify point

--- app/models/correct_answer.rb
+++ app/models/correct_answer.rb
@@ -5,7 +5,7 @@ class CorrectAnswer < ActiveRecord::Base
     a = ["a","b","c"]
     CSV.parse(tsv, :col_sep => "\t") do |row|
       CorrectAnswer.create(year:row[0],subject:row[1],number:row[2],category1:row[3],category2:row[4],
-      category3:row[5],category4:row[6],correctAnswer:row[7],image:row[8],point:[9])
+      category3:row[5],category4:row[6],correctAnswer:row[7],image:row[8],point:row[9])
     end
     return a
   end
