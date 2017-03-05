f777c81ad9b1046bb4a7baacc354f3275025ca0d
3kuni <houdig@gmail.com>
2017-03-05 10:48:10 +0900

    🐣Add update controller

--- app/controllers/correct_answers_controller.rb
+++ app/controllers/correct_answers_controller.rb
@@ -8,4 +8,19 @@ class CorrectAnswersController < ApplicationController
   def edit
     @answer = CorrectAnswer.find(params[:id])
   end
+  def update
+    @answer = CorrectAnswer.find(params[:id])
+    puts "output #{@answer} #{answer_params}"
+    if @answer.update_attributes(answer_params)
+      # 更新に成功
+      puts "success update"
+      render 'index'
+    end
+
+  end
+
+  private
+    def answer_params
+      params.require(:correct_answer).permit(:correctAnswer)
+    end
 end
